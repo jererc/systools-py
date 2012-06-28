2686446c8456151b3c41635edf393a5d69997450
Jerome Clerc <jererc@gmail.com>
2012-06-28 21:34:04 +0200

    update ssh commands for stripped down os

--- systools/network/ssh.py
+++ systools/network/ssh.py
@@ -3,6 +3,7 @@ from operator import itemgetter
 import logging
 
 from sshex import Ssh
+from sftpsync import Sftp
 
 from systools.system import PATH_UUIDS, parse_ifconfig, parse_diskutil
 
@@ -25,18 +26,30 @@ class Host(Ssh):
 
         return self.run(cmd, expects=expects, **kwargs)
 
+    def exists(self, path):
+        if self.run('ls %s' % path)[-1] == 0:
+            return True
+
     def command_exists(self, cmd):
         if self.run('type -P %s' % cmd)[-1] == 0:
             return True
 
-    def path_exists(self, path):
-        if self.run('test -d %s' % path)[-1] == 0:
+    def mkdir(self, path, use_sudo=False):
+        if self.run('mkdir %s' % path, use_sudo=use_sudo)[-1] == 0:
             return True
 
-    def mkdir(self, path, use_sudo=False):
-        cmd = 'mkdir -p %s' % path
-        if self.run(cmd, use_sudo=use_sudo)[-1] == 0:
-            return True
+    def makedirs(self, path, use_sudo=False):
+        paths = []
+        while path not in ('/', ''):
+            paths.insert(0, path)
+            path = os.path.dirname(path)
+
+        for path in paths:
+            if not self.exists(path) \
+                    and not self.mkdir(path, use_sudo=use_sudo):
+                return
+
+        return True
 
     def _udisks(self, dev, option, use_sudo=False):
         cmd = 'udisks %s %s' % (option, dev)
@@ -50,7 +63,9 @@ class Host(Ssh):
         return self._udisks(dev, '--unmount', use_sudo=True)
 
     def get_hostname(self):
-        return self.run('hostname')[0][0]
+        stdout, return_code = self.run('hostname')
+        if return_code == 0:
+            return stdout[0]
 
     def get_ifconfig(self):
         stdout = self.run('ifconfig')[0]
@@ -81,7 +96,7 @@ class Host(Ssh):
         disks = {}
 
         # Get devices and uuids
-        if self.path_exists(PATH_UUIDS):
+        if self.exists(PATH_UUIDS):
             for line in self.run('ls --color=never -l %s' % PATH_UUIDS)[0][1:]:  # skip details line
                 d, uuid, d, link = line.rsplit(None, 3)
                 dev = os.path.join('/dev', os.path.basename(link))
@@ -106,3 +121,7 @@ class Host(Ssh):
                     })
         res = sorted(res, key=itemgetter('dev'))
         return res
+
+    def sftpsync(self, *args, **kwargs):
+        sftp = Sftp(self.host, self.username, self.password, port=self.port)
+        sftp.sync(*args, **kwargs)
