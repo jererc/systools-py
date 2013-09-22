c92a09f51a684c5957765d8d325ed0d14c260365
Jerome Clerc <jererc@gmail.com>
2013-09-22 11:25:53 +0200

    clean service

--- systools/system/service.py
+++ systools/system/service.py
@@ -6,32 +6,39 @@ from systools.system import popen
 
 RE_SERVICE = {
     'start': re.compile(r'\brunning\b', re.I),
-    'stop': re.compile(r'\b(unknown instance|not running)\b', re.I),
+    'stop': re.compile(r'\b(unrecognized|unknown instance|not running)\b', re.I),
     }
 
 logger = logging.getLogger(__name__)
 
 
-def is_service_running(svc):
-    cmd = 'service %s status' % svc
-    stdout, stderr, return_code = popen(cmd)
-    output = ' '.join((stdout or []) + (stderr or []))
-    return RE_SERVICE['start'].search(output) is not None
+def _service(svc, param):
+    cmd = 'service %s %s' % (svc, param)
+    stdout, stderr, returncode = popen(cmd)
+    return ' '.join((stdout or []) + (stderr or [])), returncode
 
-def set_service(svc, status):
-    cmd = 'service %s %s' % (svc, status)
-    stdout, stderr, return_code = popen(cmd)
-    output = ' '.join((stdout or []) + (stderr or []))
-    if return_code == 0 or RE_SERVICE[status].search(output):
+def _set_service(svc, status):
+    output, returncode = _service(svc, status)
+    if returncode == 0 or RE_SERVICE[status].search(output):
         return True
     logger.error('failed to %s service %s: %s' % (status, svc, output))
     return False
 
-def check_service(svc):
-    if is_service_running(svc):
+def is_running(svc):
+    output, returncode = _service(svc, 'status')
+    return RE_SERVICE['start'].search(output) is not None
+
+def start(svc):
+    return _set_service(svc, 'start')
+
+def stop(svc):
+    return _set_service(svc, 'stop')
+
+def check_service(svc, do_start=True):
+    if is_running(svc):
         return True
     logger.error('%s service is not running', svc)
-    if set_service(svc, 'start'):
+    if do_start and start(svc):
         logger.info('started %s service', svc)
         return True
     return False
